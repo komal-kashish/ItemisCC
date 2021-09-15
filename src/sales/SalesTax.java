125a11bcd120850d0b77796b89bcd4b35be3be0b
ckk8522 <komal.kashish@tuhh.de>
2021-09-15 15:36:16 +0200

    main method introduced. Runs the input files in a loop and gives output

---
+++ src/sales/SalesTax.java
@@ -0,0 +1,27 @@
+package sales;
+import sales.model.Receipt;
+
+import java.io.FileNotFoundException;
+
+
+public class SalesTax {
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        String[] cart ={ "assets/input1.txt",
+                "assets/input2.txt",
+                "assets/input3.txt"};
+
+        for(int i=0;i<3;i++)
+        {
+            Receipt r = new Receipt(cart[i]);
+            r.TotalCost();
+            System.out.println("Output " + (i+1) + ":");
+            r.printReceipt();
+            System.out.println();
+        }
+
+    }
+
+
+}
