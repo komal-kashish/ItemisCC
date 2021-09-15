32824d98623787c983a88aaac8e8edaab1bf4ae7
ckk8522 <komal.kashish@tuhh.de>
2021-09-15 14:20:16 +0200

    Creating method to read input and parse input files

--- src/java/model/Receipt.java
+++ src/java/model/Receipt.java
@@ -1,4 +1,15 @@
 package java.model;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Receipt {
+
+    //Reads an input txt file and parsed to extract product details
+
+    public Receipt(String inputFileName)  {
+
+
+    }
 }
