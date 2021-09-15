d4d0318c73591ee2f580feb547fbeb8760cb8f73
ckk8522 <komal.kashish@tuhh.de>
2021-09-15 13:28:45 +0200

    Added fields to Item: quantity, name, price and description from ItemsDescription

--- src/java/model/Item.java
+++ src/java/model/Item.java
@@ -7,4 +7,14 @@ public class Item {
     private double cost;
     private ItemsDescription desc;
 
+    public Item(int count, String product_name, double price, ItemsDescription itemDesc){
+
+        this.quantity = count;
+        this.name = product_name;
+        this.cost = price;
+        this.desc = itemDesc;
+    }
+
+
 }
+
